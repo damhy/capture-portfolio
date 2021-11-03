58439fd974ea4c98fab8f0fcb8d149365f6647ab
Your name heredamhy <oues.hamdy@gmail.com>
2021-11-03 19:40:35 +0100

    nav and ourwork

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 //Import Pages
 
 import GlobolStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 import AboutUs from "./pages/AboutUs";
-
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//Router
+import { Switch, Route } from "react-router-dom";
 //Global Style
 
 function App() {
   return (
     <div className="App">
       <GlobolStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
