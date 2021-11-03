3724ac1d2cd3bb8a113909054b79b4576fad6bea
Your name heredamhy <oues.hamdy@gmail.com>
2021-11-03 15:40:42 +0100

    adding styles to about

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 //Import Pages
 
+import GlobolStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
+
+//Global Style
+
 function App() {
   return (
     <div className="App">
+      <GlobolStyle />
       <AboutUs />
     </div>
   );
